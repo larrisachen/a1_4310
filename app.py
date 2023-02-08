7afabb2a96a0efcb9c47293c8b6c56745209021d
LARRISA CHEN <LC949@cornell.edu>
2023-02-08 16:11:12 -0500

    update app

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from whitenoise import WhiteNoise
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='static/', 
-                          prefix='', 
-                          index_file='index.html', 
+                          prefix='static/', 
+                          index_file='index.htm', 
                           autorefresh=True)
 
 @app.route('/', methods=['GET'])
 def hello():
     return make_response("Hello, world!!!!!!!!!!!!!!!!")
-
+    
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
